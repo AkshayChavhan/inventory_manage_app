ff82847bc0b6df1850b85b055e0986a3d932d5c8
akshaychavhan <akshay@rtledgers.com>
2023-08-26 13:15:45 +0530

    userRoutes created

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,20 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv").config();
+const userRouter = require("./routes/userRoutes");
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_MONGOOSE = process.env.DB_MONGOOSE;
+
+
+
+//router middleware
+app.use("/api/users" , userRouter);
+
+
+
 
 mongoose.connect(DB_MONGOOSE ,{
     useNewUrlParser : true ,
