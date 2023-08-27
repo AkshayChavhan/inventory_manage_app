979b8252eb23d88f9b2ef48f3fe4f9b1216812db
akshaychavhan <akshay@rtledgers.com>
2023-08-27 09:53:48 +0530

    Getuser data after token verify done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser , loginUser , logoutUser , getUser } = require("../controller/userController");
+const authMiddleware = require("../authMiddleware/authMiddleware");
 
 
 router.post("/register" , registerUser);
 router.post("/login" , loginUser);
 router.post("/logout" , logoutUser);
-router.get("/getuser" , getUser)
+router.get("/getuser" , authMiddleware , getUser)
 
 module.exports =  router 
